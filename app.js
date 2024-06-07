4b4d9f8d84de1b442e89ddc15df42171856220e5
Conrado Nobre <condecnobre1995@gmail.com>
2024-06-07 20:45:14 -0300

    alterando o foco para automatico

--- app.js
+++ app.js
@@ -67,8 +67,10 @@ function reiniciarJogo() {
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled', true)
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+    input.focus(); 
 }
+
 
 
 
